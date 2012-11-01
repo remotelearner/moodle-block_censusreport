68d6b32517a14761654da27dc207beb6a91d2eec
Marko Vidberg <marko.vidberg@remote-learner.net>
2012-11-01 12:56:57 -0400

    HOSSUP-540 updated required Moodle version value in version.php

--- version.php
+++ version.php
@@ -15,5 +15,6 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 $plugin->version = 2012051802;  // The current block version (Date: YYYYMMDDXX)
-$plugin->requires = 2011120100;  // Requires this Moodle version
+$plugin->requires = 2012062500;  // Requires this Moodle version
+$plugin->maturity = MATURITY_STABLE;
 $plugin->release = "2.3.3";
