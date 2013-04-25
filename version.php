bdd30a2dccff9badc345fea0d69478ce216896f4
Tyler Bannister <tyler.bannister@remote-learner.net>
2013-04-25 13:57:35 -0400

    Updated release to 2.4.1.0

--- version.php
+++ version.php
@@ -14,7 +14,7 @@
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
-$plugin->version  = 2013022215;  // The current block version (Date: YYYYMMDDXX)
+$plugin->version  = 2013022216;  // The current block version (Date: YYYYMMDDXX)
 $plugin->requires = 2012062500;  // Requires this Moodle version
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release  = '2.4.0.0';
+$plugin->release  = '2.4.1.0';
