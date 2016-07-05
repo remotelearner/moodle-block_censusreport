7af705a909f03e5920d0242ec308d5bffae8daa9
Amy Groshek <amygroshek@gmail.com>
2016-07-05 15:54:36 -0500

    CENSUS-36 - Updated to version 2016070500 release 3.1.0.0

--- version.php
+++ version.php
@@ -14,8 +14,8 @@
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
-$plugin->version  = 2015103000;  // The current block version (Date: YYYYMMDDXX)
+$plugin->version  = 2016070500;  // The current block version (Date: YYYYMMDDXX)
 $plugin->requires = 2015101600;  // Requires this Moodle version
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release  = '3.0.0.0';
+$plugin->release  = '3.1.0.0';
 $plugin->component = 'block_censusreport';
